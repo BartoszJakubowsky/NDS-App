726b923a8e9ca8d44c3af4fe3b3a519041737e4d
Bartosz Jakubowski <bartoszj321@gmail.com>
2022-08-23 23:01:44 +0200

    next rebuild + new toDo list Leave settings by now, focus on text compiler and new methods To do: - finish method responsible for char toString (remember about reparing backspace) - add method recognizing commands - it s going to grow in time but it has to be implemented now in any form - repair method responsible for changing text color as special commands are typed
    Added:
    + method responsible for changing text color (not working)
    + method responsible for changing single characters into string (not working)
    + diverse code to main method and menu method (working at least :d)

--- Program.cs
+++ Program.cs
@@ -13,12 +13,13 @@ using System.ComponentModel;
 // - co z nim zrobić -> usunąć, przenieść, przekopiować?
 //  
 // - czy zmienić nazwę? Jak ją zmienić? (ustawić domyślnie jak katalogu)
+//
+// - dodać do menu możliwość szybkiego wyboru z przedrostkiem --
+//      - w tym celu dodać metody 
 
 
 //co zrobione
-//
-// + dodana metoda tworząca z obiektu settings plik json - validacja na starcie czy taki plik się znajduje
-// + menu wuboru na starcie programu
+// + naprawione bug z infinite right number choice at the start of program
 
 
 
@@ -26,23 +27,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Settings settings = new Settings();
-        MainProgram(settings.SettingsInit());
 
-
+        Settings settings = SettingsInit();
+        MainProgram(settings);
+        
     }
 
     public static void MainProgram(Settings settings)
     {
-        //          //
-        //   MENU   //
-        //          //
-        Console.WriteLine("\t\t\tNDS App v2\n");
-        Console.WriteLine("Copy [1] Move [2] Create Folder [3] Settings [4] Close [5]\n");
-
-        //                  //   
-        //  chosen option  //
-        //                //
+        Menu();
         int choice = ConsoleReadKey();
 
         switch (choice)
@@ -58,8 +51,8 @@ class Program
             // Settings //
             //          //
             case 4:
-
-                MainProgram();
+                settings.SetUp();
+                MainProgram(settings);
                 break;
             //          //
             //   Exit   //
@@ -74,15 +67,30 @@ class Program
 
 
     }
+
+    public static void Menu()
+    {
+        //          //
+        //   MENU   //
+        //          //
+        Console.WriteLine("\t\t\tNDS App v2\n");
+        Console.WriteLine("Copy [1] Move [2] Create Folder [3] Settings [4] Close [5]\n");
+
+        //                  //   
+        //  chosen option  //
+        //                //
+        Console.Write("Enter your choice: ");
+    }
     public static int ConsoleReadKey()
     {
         bool flag = false;
-        string _val = "";
+        string typedCharacter = "";
         int pressedNumber = 0;
+        List<string> typedCommand = new List<string>();
         ConsoleKeyInfo key;
 
 
-        Console.Write("Enter your choice: ");
+       
         do
         {
             key = Console.ReadKey(true);
@@ -90,11 +98,12 @@ class Program
             if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
             {
 
-                bool _x = int.TryParse(key.KeyChar.ToString(), out pressedNumber);
-
-
-                if (_x)
+                bool intChoice = int.TryParse(key.KeyChar.ToString(), out pressedNumber);
+               
+                if (intChoice)
                 {
+                    if (typedCharacter.Length > 0)
+                        continue;
                     //
                     //if user would get out of menu option's range
                     //
@@ -103,25 +112,43 @@ class Program
                     {
                         continue;
                     }
-                    _val += key.KeyChar;
+                    typedCharacter += key.KeyChar;
                     Console.Write(key.KeyChar);
                 }
                 else
                 {
-                    continue;
+
+                    //
+                    //if first character would be space
+                    if(typedCharacter.Length == 0)
+                        if (key.Key == ConsoleKey.Spacebar)
+                            continue;
+
+                    //add char to list
+                    typedCommand.Add(key.KeyChar.ToString());
+
+
+                    //text colour changing during special commands
+                    textColor(charToStringsCommands(typedCommand));
+
+                    //print on console
+                    typedCharacter += key.KeyChar;
+                    Console.Write(key.KeyChar);
                 }
+                
             }
-            else if (key.Key == ConsoleKey.Backspace && _val.Length > 0)
+            //else for backspace
+            else if (key.Key == ConsoleKey.Backspace && typedCharacter.Length > 0)
             {
 
-                _val = _val.Substring(0, (_val.Length - 1));
+                typedCharacter = typedCharacter.Substring(0, (typedCharacter.Length - 1));
                 Console.Write("\b \b");
             }
 
             //if user would not chose any number
             else if (key.Key == ConsoleKey.Enter)
             {
-                if (_val.Length == 0)
+                if (typedCharacter.Length == 0)
                 {
                     continue;
                 }
@@ -132,9 +159,59 @@ class Program
         // Stops Receving Keys Once Enter is Pressed
         while (!flag);
 
-
-        Console.WriteLine();
+        
+        //Console.WriteLine();
+        //foreach (var item in charToStringsCommands(typedCommand))
+        //{
+        //    Console.WriteLine(item + " ");
+        //}
         return pressedNumber;
+    }
+
+
+
+    public static void textColor(string[] input)
+    {
+
+
+        for (int i = 0; i <input.Length; i++)
+        {
+            if (input[i].Contains("-") ^ input[i].Contains("--"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            else
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+    
+    public static string[] charToStringsCommands(List<string> charInput)
+    {
+        string[] input = charInput.ToArray();
+        List<string> CoveredInput = new List<string>();
+        List<string> temp = new List<string>();
+
+        for (int i = input.Length - 1; i >= 0 ; i--)
+        {
+           
+
+            
+                if (input[i] != " ")
+                    CoveredInput.Insert(0, input[i]);
+
+                else
+                {
+                    Console.WriteLine("\n\n\n");
+                    Console.WriteLine(String.Join("", CoveredInput));
+
+                }
+
+            
+        }
+        return CoveredInput.ToArray();
+        
     }
     public static void CloseApp()
     {
@@ -196,75 +273,66 @@ class Program
         }
 
     }
-
-
-
-
-}
-/*
-public static object SettingsInit()
-{
-
-    // get current directory
-    //string currentDir = Directory.GetCurrentDirectory();
-    string currentDir = @"C:\Users\Nowe Jakubki\OneDrive\Pan_Programista\C#\# Projekty\Projekt NDS v2 - PC\NDS-App-v2";
-
-    //
-    // create search pattern for settings jsons files
-    Regex regex = new Regex("settings_[1-9]\\.json", RegexOptions.IgnoreCase);
-
-    //
-    // create list for settings files
-    List<string> settingsList = new List<string>();
-
-
-    foreach (string file in Directory.GetFiles(currentDir))
+    public static Settings SettingsInit()
     {
-        if (regex.IsMatch(file))
-        {
-            settingsList.Add(file);
-        }
-    }
-    if (settingsList.ToArray().Length > 0)
-    {
-        foreach (string json in settingsList.ToArray())
-        {
-            //download string from current directory
-            string jsonFileSerialized = File.ReadAllText(json);
-            Settings settings = JsonConvert.DeserializeObject<Settings>(jsonFileSerialized);
 
-            if (settings.DefaultSettings == "true")
+        // get current directory
+        //string currentDir = Directory.GetCurrentDirectory();
+        string currentDir = @"C:\Users\Nowe Jakubki\OneDrive\Pan_Programista\C#\# Projekty\Projekt NDS v2 - PC\NDS-App-v2";
+
+        //
+        // create search pattern for settings jsons files
+        Regex regex = new Regex("settings_[1-9]\\.json", RegexOptions.IgnoreCase);
+
+        //
+        // create list for settings files
+        List<string> settingsList = new List<string>();
+
+
+        foreach (string file in Directory.GetFiles(currentDir))
+        {
+            if (regex.IsMatch(file))
             {
-                string fileName = Path.Combine("", Path.GetFileName(json));
-                Console.WriteLine($"Settings loaded from {fileName}\n\n\n");
-                return settings;
+                settingsList.Add(file);
             }
+        }
 
+
+        if (settingsList.ToArray().Length > 0)
+        {
+            foreach (string json in settingsList.ToArray())
+            {
+                //download string from current directory
+                string jsonFileSerialized = File.ReadAllText(json);
+                Settings settings = JsonConvert.DeserializeObject<Settings>(jsonFileSerialized);
+
+                if (settings.DefaultSettings == "true")
+                {
+                    string fileName = Path.Combine("", Path.GetFileName(json));
+                    Console.WriteLine($"Settings loaded from {fileName}\n\n\n");
+                    return settings;
+                }
+
+            }
+            //actually this code shoudn't be ever executed
+            Console.WriteLine();
+            return new Settings();
 
 
 
         }
-        //actually this code shoudn't be ever executed
-        Console.WriteLine();
-        return "file not found";
-    }
-    else
-    {
-        Settings settings = new Settings()
+        else
         {
-            DefaultSettings = "true",
-            MufaPattern = "default",
-            AndSomethingElse = "default",
+            Settings settings = new Settings();
+            
 
-        };
+            //create and send string to current dir
+            string jsonFile = JsonConvert.SerializeObject(settings);
+            File.WriteAllText($@"{currentDir}\settings_1.json", jsonFile);
 
-        //create and send string to current dir
-        string jsonFile = JsonConvert.SerializeObject(settings);
-        File.WriteAllText($@"{currentDir}\settings_1.json", jsonFile);
+            return settings;
+        }
 
-        return settings;
     }
-
 }
 
-*/
