2ce321d8e60d04dd8324ba7e536d30bff6d92891
Bartosz Jakubowski <bartoszj321@gmail.com>
2022-08-27 22:52:49 +0200

    FINALLY SOMETHING WORKS + repaired char-to-string method -> unnecessary loop foreach + empty string returning when backspace + repaired color-changing method -> moved deleting code before initializing color-changing metdod + added code into color-changing code + moved methods to static class to seperate them all

---
+++ InOut.cs
@@ -0,0 +1,216 @@
+﻿using System.Text.RegularExpressions;
+
+public class InOut
+{
+
+
+    public static string[] ConsoleReadKey()
+    {
+        //flag for stopping loop
+        bool flag = false;
+
+        //pressed char displayer on console
+        string typedCharacter = "";
+
+        //for string[] of numbers to parse
+        int pressedNumber = 0;
+
+        //list for sending new chars to char-to-string method parser
+        List<string> typedCommand = new List<string>();
+
+        // :)
+        ConsoleKeyInfo key;
+
+
+
+        do
+        {
+            key = Console.ReadKey(true);
+
+            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+            {
+
+                bool intChoice = int.TryParse(key.KeyChar.ToString(), out pressedNumber);
+
+                if (intChoice)
+                {
+                    if (typedCharacter.Length > 0)
+                        continue;
+                    //
+                    //if user would get out of menu option's range
+                    //
+                    // also it's marked because of trouble to find this
+                    if (pressedNumber > 5 ^ pressedNumber == 0)
+                    {
+                        continue;
+                    }
+                    typedCharacter += key.KeyChar;
+                    Console.Write(key.KeyChar);
+                }
+                else
+                {
+
+                    //
+                    //if first character would be space
+                    if (typedCharacter.Length == 0)
+                        if (key.Key == ConsoleKey.Spacebar)
+                            continue;
+
+                    //add char to list
+                    typedCommand.Add(key.KeyChar.ToString());
+
+
+                    //text colour changing during special commands
+                    //it have to be executed after writing character
+                    TextColor(charToStringsCommands(typedCommand), key.Key);
+
+
+                    //print on console
+                    typedCharacter += key.KeyChar;
+                    Console.Write(key.KeyChar);
+
+
+                }
+
+            }
+            //else for backspace
+            else if (key.Key == ConsoleKey.Backspace && typedCharacter.Length > 0)
+            {
+                Console.Write("\b \b");
+                typedCharacter = typedCharacter.Substring(0, (typedCharacter.Length - 1));
+
+                TextColor(charToStringsCommands(typedCommand, ConsoleKey.Backspace), key.Key);
+
+            }
+
+            //exit from loop
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                //if user would not chose any number
+                if (typedCharacter.Length == 0)
+                {
+                    continue;
+                }
+                flag = true;
+                textColor();
+            }
+
+        }
+        while (!flag);
+
+        //checking if input was numberChoice or strings command
+        string[] returnChoice = charToStringsCommands(typedCommand);
+
+        if (returnChoice.Length == 0)
+        {
+            Console.WriteLine();
+            Console.Write($"działa: {pressedNumber}");
+            returnChoice = new [] { pressedNumber.ToString()};
+            return returnChoice;
+        }
+        else
+        {
+            Console.WriteLine();
+            foreach (var item in returnChoice)
+            {
+                Console.Write(item + " ");
+            }
+            return returnChoice;
+        }
+
+    }
+    //defaut random key -> it's only waiting for backspace
+	public static string[] charToStringsCommands(List<string> charInput, ConsoleKey backspace = ConsoleKey.Attention)
+	{
+
+        string[] finalArray;
+        string newItem = "";
+        List<string> temp = new List<string>();
+
+        if (backspace == ConsoleKey.Backspace)
+        {
+            charInput.RemoveAt(charInput.Count - 1);
+            newItem = String.Join("", charInput);
+            temp = newItem.Split(" ").ToList();
+
+            return finalArray = temp.ToArray();
+        }
+
+
+            newItem = String.Join("", charInput);
+            temp = newItem.Split(" ").ToList();
+
+
+        return finalArray = temp.ToArray();
+    }
+
+    public static void TextColor(string[] input, ConsoleKey Key)
+    {
+        //regex had to be use due to simplines of lastTypedWord.Contains
+        Regex regex1 = new Regex("-[a-zA-Z]+", RegexOptions.IgnoreCase);
+        Regex regex2 = new Regex("--[a-zA-Z]+", RegexOptions.IgnoreCase);
+        string lastTypedWord = input[input.Length - 1];
+
+
+        if (Key == ConsoleKey.Backspace && lastTypedWord.Length == 1 && lastTypedWord == "-")
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.CursorVisible = false;
+            Console.Write("\b");
+            Console.Write("-");
+            Console.CursorVisible = true;
+
+            return;
+        }
+        else if (lastTypedWord.Contains("--"))
+        {
+
+            //to replace one "-" with grey color
+            if (lastTypedWord.Length == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.CursorVisible = false;
+                Console.Write("\b");
+                Console.Write("-");
+                Console.CursorVisible = true;
+            }
+            else if (regex2.IsMatch(lastTypedWord))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                
+            }
+
+            
+        }
+        else if (lastTypedWord.Contains("-"))
+        {
+
+
+            if (lastTypedWord.Length == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+                //Console.CursorVisible = false;
+                ////Console.Write("\b");
+                ////Console.Write("-");
+                //Console.CursorVisible = true;
+
+            }
+            else if (regex1.IsMatch(lastTypedWord))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            }
+        }    
+        else
+        {
+            Console.ResetColor();
+        }
+    }
+
+    //for resetting color after pressing enter
+    public static void textColor()
+    {
+        Console.ResetColor();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using Newtonsoft.Json;
-using System.Text.RegularExpressions;
+﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;   
 
 
-//problemy - code z zmianą koloru textu jest szybciej wywoływany..
-// + naprawione bug z y/n
-//
-
+//InOut jako segregator na metody
 
 
 class Program
@@ -18,35 +14,11 @@ class Program
         MainProgram(settings);
 
     }
-
+        
     public static void MainProgram(Settings settings)
     {
         Menu();
-        int choice = ConsoleReadKey();
-
-        switch (choice)
-        {
-            case 1:
-                break; ;
-            case 2:
-                break;
-            case 3:
-                break;
-
-            //          //
-            // Settings //
-            //          //
-            case 4:
-                settings.SetUp();
-                MainProgram(settings);
-                break;
-            //          //
-            //   Exit   //
-            //          //
-            case 5:
-                CloseApp();
-                break;
-        }
+        string[] choice = InOut.ConsoleReadKey();
 
         MainProgram(settings);
 
@@ -67,179 +39,7 @@ class Program
         //                //
         Console.Write("Enter your choice: ");
     }
-    public static int ConsoleReadKey()
-    {
-        bool flag = false;
-        string typedCharacter = "";
-        int pressedNumber = 0;
-        List<string> typedCommand = new List<string>();
-        ConsoleKeyInfo key;
 
-
-
-        do
-        {
-            key = Console.ReadKey(true);
-
-            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-            {
-
-                bool intChoice = int.TryParse(key.KeyChar.ToString(), out pressedNumber);
-
-                if (intChoice)
-                {
-                    if (typedCharacter.Length > 0)
-                        continue;
-                    //
-                    //if user would get out of menu option's range
-                    //
-                    // also it's marked because of trouble to find this
-                    if (pressedNumber > 5 ^ pressedNumber == 0)
-                    {
-                        continue;
-                    }
-                    typedCharacter += key.KeyChar;
-                    Console.Write(key.KeyChar);
-                }
-                else
-                {
-
-                    //
-                    //if first character would be space
-                    if (typedCharacter.Length == 0)
-                        if (key.Key == ConsoleKey.Spacebar)
-                            continue;
-
-                    //add char to list
-                    typedCommand.Add(key.KeyChar.ToString());
-
-
-
-
-                    //print on console
-                    typedCharacter += key.KeyChar;
-                    Console.Write(key.KeyChar);
-
-
-                    //random consolekey 
-                    //text colour changing during special commands
-                    //it have to be executed after writing character
-                    textColor(charToStringsCommands(typedCommand, ConsoleKey.Attention));
-
-                }
-
-            }
-            //else for backspace
-            else if (key.Key == ConsoleKey.Backspace && typedCharacter.Length > 0)
-            {
-                textColor(charToStringsCommands(typedCommand, ConsoleKey.Backspace));
-
-                typedCharacter = typedCharacter.Substring(0, (typedCharacter.Length - 1));
-                Console.Write("\b \b");
-            }
-
-            //if user would not chose any number
-            else if (key.Key == ConsoleKey.Enter)
-            {
-                if (typedCharacter.Length == 0)
-                {
-                    continue;
-                }
-                flag = true;
-            }
-
-        }
-        // Stops Receving Keys Once Enter is Pressed
-        while (!flag);
-
-
-        //Console.WriteLine();
-        //foreach (var item in charToStringsCommands(typedCommand))
-        //{
-        //    Console.WriteLine(item + " ");
-        //}
-        return pressedNumber;
-    }
-
-
-
-    public static void textColor(string[] input)
-    {
-        //regex had to be use due to simplines of lastTypedWord.Contains
-        Regex regex1 = new Regex("-[a-zA-Z]+", RegexOptions.IgnoreCase);
-        Regex regex2 = new Regex("--[a-zA-Z]+", RegexOptions.IgnoreCase);
-        string lastTypedWord = input[input.Length - 1];
-
-        if (lastTypedWord.Contains("--"))
-        {
-
-            //to replace one "-" with grey color
-            if (lastTypedWord.Length == 2)
-            {
-                Console.CursorVisible = false;
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.Write("\b\b");
-                Console.Write("--");
-                Console.CursorVisible = true;
-            }
-            else if (regex2.IsMatch(lastTypedWord))
-            {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-            }
-
-        }
-        else if (lastTypedWord.Contains("-"))
-        {
-
-            if (lastTypedWord.Length == 1)
-            {
-                Console.CursorVisible = false;
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                Console.Write("\b");
-                Console.Write("-");
-                Console.CursorVisible = true;
-            }
-            else if (regex1.IsMatch(lastTypedWord))
-            {
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            }
-        }
-        else if (lastTypedWord == "") 
-        {
-
-            return;
-        }
-        else
-        {
-            Console.ResetColor();
-        }
-    }
-
-    public static string[] charToStringsCommands(List<string> charInput, ConsoleKey backspace)
-    {
-
-        string[] finalArray;
-        string newItem;
-        List<string> temp = new List<string>();
-
-        if (backspace == ConsoleKey.Backspace)
-        {
-            charInput.RemoveAt(charInput.Count - 1);
-            newItem = String.Join("", temp);
-            temp = newItem.Split(" ").ToList();
-
-            return finalArray = temp.ToArray();
-        }
-
-        foreach (var item in charInput)
-        {
-            newItem = String.Join("", charInput);
-            temp = newItem.Split(" ").ToList();
-        }
-
-        return finalArray = temp.ToArray();
-
-    }
     public static void CloseApp()
     {
         //napisać kod, który sprawdza czy wszystko jest cacy
@@ -305,7 +105,7 @@ class Program
 
         // get current directory
         //string currentDir = Directory.GetCurrentDirectory();
-        string currentDir = @"C:\Users\Nowe Jakubki\OneDrive\Pan_Programista\C#\# Projekty\Projekt NDS v2 - PC\NDS-App-v2";
+        string currentDir = @"C:\Users\barto\OneDrive\Pan_Programista\C#\# Projekty\Projekt NDS v2 - PC\NDS-App-v2";
 
         //
         // create search pattern for settings jsons files
